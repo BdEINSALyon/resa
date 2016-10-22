031053510243e2b5da2363b2a789ff46037e728c
Gabriel Augendre <gabriel@augendre.info>
2016-10-22 18:48:33 +0200

    Change URL for occurrence add

--- bookings/urls.py
+++ bookings/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'^resource-category/(?P<id>[0-9]+)$', ResourceCategoryDayView.as_view(), name='resource-category-day'),
     url(r'^booking/(?P<pk>[0-9]+)$', BookingDetailView.as_view(), name='booking-details'),
     url(r'^booking/(?P<pk>[0-9]+)/edit$', BookingUpdateView.as_view(), name='booking-update'),
-    url(r'^booking/(?P<booking_pk>[0-9]+)/occurrences/new$',
+    url(r'^booking/(?P<booking_pk>[0-9]+)/occurrence/new$',
         BookingOccurrenceCreateView.as_view(),
         name='occurrence-new'),
 ]
