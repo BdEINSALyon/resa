6c3c3dfc3514cc6040b375565ed846f96f6e9979
Gabriel Augendre <gabriel@augendre.info>
2016-10-22 18:41:47 +0200

    Add some options for datetime picker

--- bookings/forms.py
+++ bookings/forms.py
@@ -6,18 +6,22 @@ from bookings.models import BookingOccurrence
 
 class BookingOccurrenceForm(forms.ModelForm):
     class Meta:
+        picker_options = {
+            "format": "DD/MM/YYYY HH:mm",
+            "sideBySide": True,
+            "calendarWeeks": True,
+            "toolbarPlacement": 'top',
+            "showTodayButton": True,
+            "showClear": True,
+            "showClose": True,
+        }
         model = BookingOccurrence
         fields = ['start', 'end']
         widgets = {
             'start': DateTimePicker(
-                options={
-                    "format": "DD/MM/YYYY HH:mm"
-                }
+                options=picker_options
             ),
             'end': DateTimePicker(
-                options={
-                    "format": "DD/MM/YYYY HH:mm"
-                }
+                options=picker_options
             ),
         }
-
