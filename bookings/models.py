32e7a4b0ccc0426d80dcc3002662fec73d98759a
Gabriel Augendre <gabriel@augendre.info>
2016-10-25 18:56:02 +0200

    Show badge for number of booked resources

--- bookings/models.py
+++ bookings/models.py
@@ -239,7 +239,7 @@ class BookingOccurrence(models.Model):
         return self.start < other.start
 
     def resources_names(self):
-        total_count = self.resources.count()
+        total_count = self.get_resources_count()
         count = 5
         queryset = self.resources.all()[:count]
         resources = [r.name for r in queryset]
@@ -247,6 +247,9 @@ class BookingOccurrence(models.Model):
             resources.append('...')
 
         return resources
+
+    def get_resources_count(self):
+        return self.resources.count()
 
 
 class ResourceLock(models.Model):
