0551e3acdcac14093146d96dda57162f14a89b20
Gabriel Augendre <gabriel@augendre.info>
2016-10-31 20:27:59 +0100

    Fix run script

--- bash/run-prod.sh
+++ bash/run-prod.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python manage.py migrate && python manage.py collectstatic --noinput && gunicorn resa.wsgi -b 0.0.0.0:8000
+python manage.py migrate && python manage.py collectstatic --noinput && gunicorn resa.wsgi --log-file -
