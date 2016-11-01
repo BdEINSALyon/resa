3fb7d29d717916ff2084305243fc44dfac6d84ad
Gabriel Augendre <gabriel@augendre.info>
2016-11-01 11:25:22 +0100

    Impose formatting for datetime in occurrence

--- bookings/forms.py
+++ bookings/forms.py
@@ -2,6 +2,7 @@ import logging
 
 from bootstrap3_datetime.widgets import DateTimePicker
 from django import forms
+from django.forms import DateTimeField
 from django.utils.translation import ugettext_lazy as _
 
 from bookings.models import BookingOccurrence, Booking, Resource
@@ -11,27 +12,34 @@ log = logging.getLogger(__name__)
 
 class BookingOccurrenceForm(forms.ModelForm):
     class Meta:
-        picker_options = {
-            "format": "DD/MM/YYYY HH:mm",
-            "sideBySide": True,
-            "calendarWeeks": True,
-            "widgetPositioning": {
-                "vertical": "top"
-            },
-        }
         model = BookingOccurrence
         fields = ['start', 'end', 'resources']
         widgets = {
-            'start': DateTimePicker(
-                options=picker_options
-            ),
-            'end': DateTimePicker(
-                options=picker_options
-            ),
             'resources': forms.SelectMultiple(
                 attrs={'size': 10}
             ),
         }
+
+    picker_options = {
+        "format": "DD/MM/YYYY HH:mm",
+        "sideBySide": True,
+        "calendarWeeks": True,
+        "widgetPositioning": {
+            "vertical": "top"
+        },
+    }
+
+    start = DateTimeField(
+        input_formats=['%d/%m/%Y %H:%M'],
+        widget=DateTimePicker(options=picker_options),
+        label=BookingOccurrence._meta.get_field('start').verbose_name.capitalize()
+    )
+
+    end = DateTimeField(
+        input_formats=['%d/%m/%Y %H:%M'],
+        widget=DateTimePicker(options=picker_options),
+        label=BookingOccurrence._meta.get_field('end').verbose_name.capitalize()
+    )
 
     def __init__(self, *args, **kwargs):
         self.form_booking_id = kwargs.pop("booking_pk", None)
