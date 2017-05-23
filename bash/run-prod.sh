caefab5b85ebfaca685825a84aaee7663e30843d
Gabriel Augendre <gabriel@augendre.info>
2017-05-23 17:54:09 +0200

    Answer yes to migrations

--- bash/run-prod.sh
+++ bash/run-prod.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python3 manage.py migrate && python3 manage.py collectstatic --noinput && gunicorn resa.wsgi -b 0.0.0.0:8000 --log-file -
+yes yes | python3 manage.py migrate && python3 manage.py collectstatic --noinput && gunicorn resa.wsgi -b 0.0.0.0:8000 --log-file -
